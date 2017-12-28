fbca266cce04da030af82d7eda97f15f67a6a933
ffisherr <32161251+ffisherr@users.noreply.github.com>
2017-12-28 22:39:53 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,6 +1,19 @@
 import json
+import argparse
 import sys
+import os
 from math import sqrt
+
+
+def createParser():
+    parser = argparse.ArgumentParser(description='Поиск бара')
+    parser.add_argument('longitude',
+                        type=float, nargs='+', help='It is your coordinates')
+    parser.add_argument('latitude',
+                        type=float, nargs='+', help='It is your coordinates')
+    parser.add_argument('filepath',
+                        type=str, nargs='+', help='Way to json file with data')
+    return parser
 
 
 def get_distance(user_lon, user_lat, bar_lon, bar_lat):
@@ -12,59 +25,72 @@ def get_distance(user_lon, user_lat, bar_lon, bar_lat):
 
 def seat_counter(bars):
     seats_count = []
-    for bar in bars['features']:
+    for bar in bars:
         seats_count.append(
-            [bar.get('properties').get('Attributes').get('SeatsCount'),
-             bar.get('properties').get('Attributes').get('Name')]
+            bar.get('properties').get('Attributes').get('SeatsCount')
         )
     return seats_count
 
 
 def load_json_data_from_file(filepath):
-    if os.path.exists(filepath):
-        try:
-            with open(filepath, 'r', encoding='utf-8') as json_file:
-                json_data = json.loads(json_file.read())
-            return json_data
-        except ValueError:
-            print('Ошибка переобразования JSON файла')
-            return None
-    else:
-        print('Файла {} не найдено!'.format(filepath))
+    with open(filepath, 'r', encoding='utf-8') as json_file:
+            json_data = json.loads(json_file.read())
+    return json_data
+
+
+def looking_the_same(bars_data, this_bar):
+    bars = []
+    for bar in bars_data:
+        seats = bar.get('properties').get('Attributes').get('SeatsCount')
+        if this_bar == seats:
+            bars.append(bar)
+    return bars
 
 
 def get_biggest_bar(bars_data):
     seats_count = seat_counter(bars_data)
     biggest_bar = max(seats_count)
-    biggest_bar_name = biggest_bar[1]
-    print(biggest_bar_name)
+    return looking_the_same(bars_data, biggest_bar)
 
 
 def get_smallest_bar(bars_data):
     seats_count = seat_counter(bars_data)
     smallest_bar = min(seats_count)
-    smallest_bar_name = smallest_bar[1]
-    print(smallest_bar_name)
+    return looking_the_same(bars_data, smallest_bar)
 
 
 def get_closest_bar(bars_data, longitude, latitude):
     distance_to_bars = []
-    for bar in bars_data['features']:
+    for bar in bars_data:
         lon2 = bar.get('geometry').get('coordinates')[0]
         lat2 = bar.get('geometry').get('coordinates')[1]
         distance_to_bars.append(
-            [get_distance(longitude, latitude, lon2, lat2),
-             bar.get('properties').get('Attributes').get('Name')])
+            get_distance(longitude, latitude, lon2, lat2))
     closest_bar = min(distance_to_bars)
-    closest_bar_name = closest_bar[1]
-    print(closest_bar_name)
+    bars = []
+    for bar in bars_data:
+        lon2 = bar.get('geometry').get('coordinates')[0]
+        lat2 = bar.get('geometry').get('coordinates')[1]
+        distance = get_distance(longitude, latitude, lon2, lat2)
+        if distance == closest_bar:
+            bars.append(bar)
+    return bars
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        json_data = load_json_data_from_file('bars.json')
-        get_closest_bar(json_data, sys.argv[1], sys.argv[2])
-        get_biggest_bar(json_data)
-        get_smallest_bar(json_data)
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    if os.path.exists(namespace.filepath[0]):
+        try:
+            json_data = load_json_data_from_file(namespace.filepath[0])
+            print(get_closest_bar(
+                json_data['features'],
+                namespace.longitude[0],
+                namespace.latitude[0])
+            )
+            print(get_biggest_bar(json_data['features']))
+            print(get_smallest_bar(json_data['features']))
+        except ValueError:
+                print('Ошибка переобразования JSON файла')
     else:
-        print('Укажите верный путь к файлу')
+        print('Файла {} не найдено!'.format(namespace.filepath[0]))
